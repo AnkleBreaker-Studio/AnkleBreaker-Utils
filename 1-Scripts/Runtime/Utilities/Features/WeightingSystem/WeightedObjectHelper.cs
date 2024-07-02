cf679b1adb6b02020bd4bb747204621403cb7502
XuX26 <ulysse.raillon@hotmail.fr>
2024-07-02 12:49:24 +0200

    [FEAT] Add Random + other features scripts (Weighting, MaterialApplier, ObjectFinder)
    [CLEAN] Split scripts in 2 folders : Global and Features

---
+++ 1-Scripts/Runtime/Utilities/Features/WeightingSystem/WeightedObjectHelper.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AnkleBreaker.Utils.Weighting
+{
+    public static class WeightedObjectHelper
+    {
+        public static T GetRandomObject<T>(List<T> weightObjects,System.Random rnd, float totalWeight) where T : IWeightObject
+        {
+            if (weightObjects == null)
+                return default;
+
+            float diceRoll = (float)(rnd.NextDouble()) * totalWeight;
+            
+            foreach (T prefabWeight in weightObjects)
+            {
+                if (prefabWeight.Weight >= diceRoll)
+                    return prefabWeight;
+                diceRoll -= prefabWeight.Weight;
+            }
+
+            return default;
+        }
+        
+        public static T GetRandomObject<T>(List<T> weightObjects, float totalWeight) where T : IWeightObject
+        {
+            if (weightObjects == null)
+                return default;
+            
+            float diceRoll = UnityEngine.Random.Range(0f, totalWeight);
+            foreach (T prefabWeight in weightObjects)
+            {
+                if (prefabWeight.Weight >= diceRoll)
+                    return prefabWeight;
+                diceRoll -= prefabWeight.Weight;
+            }
+
+            return default;
+        }
+        
+        public static T GetRandomObject<T>(List<T> weightObjects) where T : IWeightObject
+        {
+            if (weightObjects == null)
+                return default;
+            
+            float totalWeight = weightObjects.Sum(p => p.Weight);
+
+            return GetRandomObject(weightObjects, totalWeight);
+        }
+    }
+}

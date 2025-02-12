d743e712a65934108b5a165a8ca3a3971d5a0c68
Timothee Le Corre <timothee.le.corre@grimasgames.fr>
2025-02-12 12:01:38 +0100

    [FEAT] MapUtils : Add get map size, center with height from terrains

--- 1-Scripts/Runtime/Utilities/Features/Map/MapUtils.cs
+++ 1-Scripts/Runtime/Utilities/Features/Map/MapUtils.cs
@@ -10,13 +10,26 @@ namespace AnkleBreaker.Utils.Map
         /// </summary>
         public static void CalculateSizeAndCenterFromTerrainsInScene(out Vector2 size, out Vector2 center)
         {
+            CalculateSizeAndCenterFromTerrainsInScene(out size, out Vector3 vector3Center, out float _);
+            
+            center = new Vector2(vector3Center.x, vector3Center.z);
+        }
+        
+        /// <summary>
+        /// Calculate the size and center of the map from terrains in the scene.
+        /// WARNING: This solution might not work correctly if your terrains are not set up in a grid.
+        /// </summary>
+        public static void CalculateSizeAndCenterFromTerrainsInScene(out Vector2 size, out Vector3 center,out float height)
+        {
             // Find all terrains in the scene
-            Terrain[] terrains = Object.FindObjectsOfType<Terrain>(true);
+            Terrain[] terrains = Object.FindObjectsByType<Terrain>(FindObjectsInactive.Include,FindObjectsSortMode.None);
 
             float minX = float.MaxValue;
             float minZ = float.MaxValue;
             float maxX = float.MinValue;
             float maxZ = float.MinValue;
+            float maxHeight = float.MinValue;
+            float minHeight = float.MaxValue;
 
             foreach (Terrain terrain in terrains)
             {
@@ -27,13 +40,16 @@ namespace AnkleBreaker.Utils.Map
                 minZ = Mathf.Min(minZ, position.z);
                 maxX = Mathf.Max(maxX, position.x + localSize.x);
                 maxZ = Mathf.Max(maxZ, position.z + localSize.z);
+                maxHeight = Mathf.Max(maxHeight, position.y + localSize.y);
+                minHeight = Mathf.Min(minHeight, position.y);
             }
 
             float mapWidth = maxX - minX;
             float mapLength = maxZ - minZ;
+            height = maxHeight - minHeight;
 
             size = new Vector2(mapWidth, mapLength);
-            center = new Vector2(minX + mapWidth / 2, minZ + mapLength / 2);
+            center = new Vector3(minX + mapWidth / 2,minHeight + height / 2, minZ + mapLength / 2);
         }
     }
 }
